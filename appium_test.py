f4c0ce8d3de0b1ca671e718814a76449f6b22c59
KnTa <a25351472@hotmail.com>
2019-06-07 15:51:07 +0800

    backup
    backup

---
+++ appium_test.py
@@ -0,0 +1,61 @@
+import sys, time
+import unittest
+from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+
+def passIntro(actions):
+  time.sleep(5)
+  for i in range(1,7):	    
+    actions.press(x=700,y=1200).release().perform()
+    time.sleep(2)
+  
+  time.sleep(2)
+  actions.press(x=600,y=1210).release().perform()
+  time.sleep(5)
+  
+class AppTest(unittest.TestCase): 
+   
+  def setUp(self):
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '5.1.1'
+    desired_caps['deviceName'] = 'android-48d224b0497f4952'
+    desired_caps['appPackage'] = 'it.feio.android.omninotes'
+    desired_caps['appActivity'] = '.MainActivity'
+
+    self.wd = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    self.wd.implicitly_wait(60)
+    self.actions = TouchAction(self.wd)
+	
+    passIntro(self.actions)  
+
+  def create_note(self, title, content):
+    self.wd.find_element_by_id('it.feio.android.omninotes:id/fab_expand_menu_button').click()  
+    time.sleep(1)
+	#click Article Comment 
+    self.actions.press(x=660,y=1100).release().perform() 
+    time.sleep(1)
+    self.wd.find_element_by_id('it.feio.android.omninotes:id/detail_title').set_value(title)
+    self.wd.find_element_by_id('it.feio.android.omninotes:id/detail_content').set_value(content)
+    time.sleep(1)
+    #click back
+    self.actions.press(x=40,y=90).release().perform()   
+    time.sleep(2)
+	
+  def test_createCategory_1(self):
+    test_text = 'aaa'
+	
+    AppTest.create_note(self, 'ccc', '123')
+
+    self.actions.press(x=400,y=200).release().perform()
+    time.sleep(2)
+    #self.wd.find_element_by_id('it.feio.android.omninotes:id/menu_category').click
+    self.actions.press(x=500,y=100).release().perform()
+    time.sleep(2)	
+    self.wd.find_element_by_id('it.feio.android.omninotes:id/buttonDefaultPositive').click
+    time.sleep(2)
+    self.wd.find_element_by_id('it.feio.android.omninotes:id/category_title').set_value(test_text)
+    self.wd.find_element_by_id('it.feio.android.omninotes:id/save').click
+
+if __name__ == "__main__":
+  unittest.main()
